467633bd17b279c57adaa76989e5b0ea22ca8cd9
Nardi <700nardi@gmail.com>
2024-11-12 15:15:30 +0800

    add test

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <h1 className="text-3xl text-violet-700 font-black">
-      Welcome to the world of Next.js
+      Welcome to the world of Next.js test
     </h1>
   );
 }
